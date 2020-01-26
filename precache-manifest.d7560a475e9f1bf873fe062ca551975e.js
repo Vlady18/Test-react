f1a00fc44fee2c96abdd4122727771a655cc79b8
Mazharenko <mazhor02@gmail.com>
2020-01-26 19:08:19 +0200

    first-com

---
+++ precache-manifest.d7560a475e9f1bf873fe062ca551975e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1ced8e93c60e6efe62b5",
+    "url": "/static/js/main.1ced8e93.chunk.js"
+  },
+  {
+    "revision": "00b31b69a913ecaa27e6",
+    "url": "/static/js/1.00b31b69.chunk.js"
+  },
+  {
+    "revision": "1ced8e93c60e6efe62b5",
+    "url": "/static/css/main.4b69615d.chunk.css"
+  },
+  {
+    "revision": "b77f3028ec3975afe1c070f5d4e0eb33",
+    "url": "/index.html"
+  }
+];
